f76152a07cf94798a166e7d4a704c88ca758802f
shubham <tonygupta275@gmail.com>
2024-05-30 22:16:04 +0530

    Add

--- backend/server/Routes/apiroute.js
+++ backend/server/Routes/apiroute.js
@@ -7,4 +7,13 @@ router.post("/admin/addCommittee",addCommittee);
 router.get("/admin/allCommittee",allCommittee);
 router.get("/admin/singleCommittee",singleCommittee);
 
+
+//place this route in the end of all of route
+router.all("**", (req, res)=>{
+    res.send({
+        success:false,
+        status:404,
+        message:"Invalid Address"
+    })
+})
 export default router;
